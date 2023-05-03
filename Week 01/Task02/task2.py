c612d0e2d72ac0f87765b644c9519e4374adbacb
Xuanting85 <xuanting2000@gmail.com>
2023-05-03 17:33:28 +0800

    updates to tasks 1 to 3

--- Week 01/Task02/task2.py
+++ Week 01/Task02/task2.py
@@ -2,57 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-
 def createacovariance():
-
-  rng = np.random.default_rng()
-  a = rng.random(size=(3, 3))
-
-  s1 = np.matmul(a.T, a)
-
-  w, v = np.linalg.eig(s1)
-
-  # you can play with the non-zero values
-  d = np.array([[4, 0, 0], [0, 1, 0], [0, 0, 0.25]])
-
+  rng = np.random.default_rng(seed=12)
+  a = rng.random(size=(3,3))
+  s1 =np.matmul(a.T,a)
+  w,v = np.linalg.eig(s1)
+  d = np.array([[4,0,0], [0,1,0], [0,0,0.25]]) #you can play with the non-zero values
   a = v.T @ d @ v
   print(a)
   return a
 
 
 def plotdirvariances(S):
-
   t = np.linspace(0, 2*np.pi, 50)
   u = np.linspace(0, 2*np.pi, 50)
   T, U = np.meshgrid(t, u)
-
-  V = np.array([np.cos(U) * np.sin(T), np.cos(U) * np.cos(T), np.sin(U)])
-
-  print(V.shape)  # (3,50,50)
-
+  V= np.array([np.cos(U) * np.sin(T), np.cos(U) * np.cos(T), np.sin(U)])  
+  print(V.shape) #(3,50,50)
+  
   scaledv = np.zeros_like(V)
   for i in range(V.shape[1]):
     for j in range(V.shape[2]):
-      thisv = V[:, i, j]
+      thisv = V[:,i,j]
 
-      # TODO need to compute v^T S v
-      stv = np.dot(thisv.T, S)
-      #TODO multiply it to it
-      scaledv[:, i, j] = np.dot(stv, thisv) *thisv
-
-      #scaledv[:,i,j]= stv * V[:,i,j] 
-      
-      
-  fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-  #surf = ax.plot_surface(V[0,:], V[1,:], V[2,:] , antialiased=False, color=(0.9,0.9,0.0,0.2))
-
-  surf = ax.plot_surface(scaledv[0,:,:], scaledv[1,:,:], scaledv[2,:,:],
-                       antialiased=False, color=(0.2,0.9,0.0,0.2))
-
-  plt.show()
+      #TODO need to compute v^T S v
+      scaledv[:,i,j]= thisv * np.sqrt(thisv.T @ S @ thisv)
   
+  fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+  surf = ax.plot_surface(scaledv[0,:,:], scaledv[1,:,:], scaledv[2,:,:],
+                       antialiased=False, color=(0.9,0.9,0.0,0.2))
+  plt.show()
+
 if __name__=='__main__':
   S = createacovariance()
   plotdirvariances(S)
-
   
---
+++ Week 01/Task03/task3.py
@@ -0,0 +1,44 @@
+import os
+import torch
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+folder = r'C:\Users\Xuanting\Desktop\data'
+num_exp = 2
+num_lr = 5
+num_reps = 10
+
+# Initialize an empty list to store the data from all files
+data_list = []
+
+# Iterate over all experiment, learning rate, and repetition indices
+for exp in range(num_exp):
+    for lr in range(num_lr):
+        for rep in range(num_reps):
+            # Construct the filename and file path for the current file
+            filename = 'exp{:d}_lrind{:d}_run{:d}_acc.pt'.format(exp+1, lr, rep)
+            filepath = os.path.join(folder, filename)
+
+            # Load the data from the file using PyTorch's torch.load() function
+            with open(filepath, 'rb') as f:
+                data = torch.load(f)
+
+            # Append the data to the list of data
+            data_list.append({'experiment': exp+1, 'learning_rate': lr, 'repetition': rep, 'accuracy': data.item()})
+
+# Convert the list of data to a pandas dataframe
+df = pd.DataFrame(data_list)
+
+# Set the seaborn style
+sns.set_style("whitegrid")
+
+# Plot the accuracy values for experiment 1
+sns.lineplot(data=df[df['experiment'] == 1], x='learning_rate', y='accuracy',
+             errorbar='sd', label='Experiment 1')
+
+# Plot the accuracy values for experiment 2
+sns.lineplot(data=df[df['experiment'] == 2], x='learning_rate', y='accuracy',
+             errorbar='sd', label='Experiment 2')
+
+plt.show()
