607bf47c1f2f85e96f6218e08886acc432eb31db
Xuanting85 <xuanting2000@gmail.com>
2023-05-03 13:38:00 +0800

    pushed task1

--- Week 01/Task01/task1.py
+++ Week 01/Task01/task1.py
@@ -1,86 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 def createacovariance():
-
-  rng = np.random.default_rng()
-  a= rng.random(size=(3,3))
-  
-  s1=np.matmul( a.T,a )
-  
-  w,v = np.linalg.eig(s1)
-  
-  d= np.array ([ [4,0,0], [0,1,0], [0,0,0.25]  ])
-  
-  a = v.T @ d @ v
-  print(a)
-  return a
-
-
-#from https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
-
-def set_axes_equal(ax):
-    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-    cubes as cubes, etc..  This is one possible solution to Matplotlib's
-    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
-
-    Input
-      ax: a matplotlib axis, e.g., as output from plt.gca().
-    '''
-
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
+    rng = np.random.default_rng()
+    A = rng.random(size=(3,3))
+    S = np.matmul(A.T, A)
+    w, v = np.linalg.eig(S)
+    D = np.array([[4, 0, 0], [0, 1, 0], [0, 0, 0.25]])
+    cov = v.T @ D @ v
+    return cov
 
 def plotellipse():
+    cov = createacovariance()
+    mean = np.zeros(3)
+    num_points = 50
+    t = np.linspace(0, 2*np.pi, num_points)
+    u = np.linspace(0, np.pi, num_points)
+    T, U = np.meshgrid(t, u)
+    X = np.cos(T) * np.sin(U)
+    Y = np.sin(T) * np.sin(U)
+    Z = np.cos(U)
+    XYZ = np.array([X.flatten(), Y.flatten(), Z.flatten()])
+    scaled_XYZ = np.linalg.cholesky(cov) @ XYZ
+    scaled_X = np.reshape(scaled_XYZ[0,:], (num_points, num_points)) + mean[0]
+    scaled_Y = np.reshape(scaled_XYZ[1,:], (num_points, num_points)) + mean[1]
+    scaled_Z = np.reshape(scaled_XYZ[2,:], (num_points, num_points)) + mean[2]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(scaled_X, scaled_Y, scaled_Z, alpha=0.5)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
 
-  
-  t = np.linspace(0, 2*np.pi, 50)
-  u = np.linspace(0, 2*np.pi, 50)
-  T, U = np.meshgrid(t, u)
-  
-  V= np.array( [np.cos(U) * np.sin(T), np.cos(U) * np.cos(T), np.sin(U)] )
-
-  
-  print(V.shape) #(3,50,50)
-  
-  scaledv = np.zeros_like(V)
-  scalers=np.array([1.0, 4.0, 9.0])
-  
-  for i in range(V.shape[1]):
-    for j in range(V.shape[2]):
-      
-      scaledv[:,i,j]= V[:,i,j]*scalers
-  
-  fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-  #surf = ax.plot_surface(V[0,:], V[1,:], V[2,:], antialiased=False, color=(0.9,0.9,0.0,0.2))
-
-  surf = ax.plot_surface(scaledv[0,:,:], scaledv[1,:,:], scaledv[2,:,:],
-                       antialiased=False, color=(0.9,0.9,0.0,0.2))
-
-  set_axes_equal(ax) #optional
-  plt.show()  
-
-  
 if __name__=='__main__':
-
-  plotellipse()
-  #plotcircle2()
-  
+    plotellipse()
